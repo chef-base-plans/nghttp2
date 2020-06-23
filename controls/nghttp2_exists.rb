3765fb148a7bfdb339bc49d1f548a4be8cd0eceb
Steven Marshall <SMarshall@chef.io>
2020-06-23 12:16:44 +0100

    [nghttp2] Various refactors to existing code
    Added inspec.lock to .gitignore
    Updated README.md to latest format
    Added plan_name to attributes.yml
    Updated controls to change description
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/nghttp2_exists.rb
+++ controls/nghttp2_exists.rb
@@ -8,31 +8,35 @@ control 'core-plans-nghttp2' do
   impact 1.0
   title 'Ensure nghttp2 libraries exist as expected'
   desc '
-  We check that the directories that nghttp2 installs are present.
+  To test that the libraries that nghttp2 export are in the correct file path, we first find the file path for the package.
+  Using this file path we then check for the existance of the directories at the expected location.
+    $ ls -al $PKG_PATH/include/
+      . .. nghttp2/
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty}
-  end
-
-  describe command("ls #{File.join(hab_pkg_path.stdout.strip, "include")}") do
-    its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  describe command("ls #{File.join(hab_pkg_path.stdout.strip, "lib")}") do
+  describe command("ls -al #{File.join(hab_pkg_path.stdout.strip, "include")}") do
     its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  describe command("ls #{File.join(hab_pkg_path.stdout.strip, "share")}") do
+  describe command("ls -al #{File.join(hab_pkg_path.stdout.strip, "lib")}") do
     its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
+
+  describe command("ls -al #{File.join(hab_pkg_path.stdout.strip, "share")}") do
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+
 end
