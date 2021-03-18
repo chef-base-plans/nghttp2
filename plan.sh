efd0c094e7e6abc54d9f5c50736365a30f60fb38
rybons <bonnerrk@gmail.com>
2021-03-18 15:30:44 -0400

    Bumped version 1.40.0 -> 1.43.0

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nghttp2
 pkg_origin=core
-pkg_version=1.40.0
+pkg_version=1.43.0
 pkg_description="nghttp2 is an open source HTTP/2 C Library."
 pkg_upstream_url=https://nghttp2.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=eb9d9046495a49dd40c7ef5d6c9907b51e5a6b320ea6e2add11eb8b52c982c47
+pkg_shasum=6c849675ef1314130cec93197fbac49c848a40f8d7c000cf4af276d6efbdba0b
 pkg_build_deps=(
   core/make
   core/gcc
