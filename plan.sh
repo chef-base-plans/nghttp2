61cef2ff907c29c2e6990b75ba72cf84282acc98
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-03-22 13:21:45 +0530

    Checksum value updated

--- plan.sh
+++ plan.sh
@@ -6,7 +6,7 @@ pkg_upstream_url=https://nghttp2.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=6c849675ef1314130cec93197fbac49c848a40f8d7c000cf4af276d6efbdba0b
+pkg_shasum=45cc3ed91966551f92b31958ceca9b3a9f23ce4faf5cbedb78aa3327cd4e5907
 pkg_build_deps=(
   core/make
   core/gcc
