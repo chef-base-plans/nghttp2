e87db3c1a26559e3a7719cdf5fdb86509ef6bed3
Nimit <nimit.jyotiana@hotmail.com>
2021-10-27 11:32:17 +0530

    upgraded version to v1.46.0
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nghttp2
 pkg_origin=core
-pkg_version=1.43.0
+pkg_version=1.46.0
 pkg_description="nghttp2 is an open source HTTP/2 C Library."
 pkg_upstream_url=https://nghttp2.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=45cc3ed91966551f92b31958ceca9b3a9f23ce4faf5cbedb78aa3327cd4e5907
+pkg_shasum=4b6d11c85f2638531d1327fe1ed28c1e386144e8841176c04153ed32a4878208
 pkg_build_deps=(
   core/make
   core/gcc
